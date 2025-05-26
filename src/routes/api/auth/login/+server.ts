533c16984e54953de435c406e63e566c94956e3e
rhsyadav <y.kaushal161183@gmail.com>
2025-05-26 23:52:41 +0100

    test

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,20 +1,21 @@
-// import { API_BASE_URL } from "$lib/constants/env.config";
-// import type { RequestHandler } from "./$types";
+import { API_BASE_URL } from "$lib/constants/env.config";
+import type { RequestHandler } from "./$types";
 
-// export const POST: RequestHandler = async ({ request }) => {
-//     const body = await request.json();
-// 	const res = await fetch(`${API_BASE_URL}/auth/login`, {
-// 		method: "POST",
-// 		body: JSON.stringify(body),
-// 		headers: {
-// 			"Content-Type": "application/json",
-// 		},
-// 		credentials: "include",
-// 	});
+export const POST: RequestHandler = async ({ request }) => {
+    const body = await request.json();
+    console.log("Server Login API_BASE_URL", API_BASE_URL);
+	const res = await fetch(`${API_BASE_URL}/auth/login`, {
+		method: "POST",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: "include",
+	});
 
-// 	// console.log("Server Response", res);
-// 	return new Response(res.body, {
-// 		status: res.status,
-// 		headers: res.headers,
-// 	});
-// };
+	console.log("Server Response", res);
+	return new Response(res.body, {
+		status: res.status,
+		headers: res.headers,
+	});
+};
