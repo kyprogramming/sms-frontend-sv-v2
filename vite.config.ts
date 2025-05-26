a91f43fd8539b13354e6bb292617b85ccd169d96
rhsyadav <y.kaushal161183@gmail.com>
2025-05-26 20:52:52 +0100

    rewrite logic changed

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,13 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://school-management-api-izm4.onrender.com",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, "/api"),
+			},
+		},
+	},
 });
